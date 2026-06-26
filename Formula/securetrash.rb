f060986233a66298acfd93d43b8aede14fd5f914
Di-kairos <205350801+Di-kairos@users.noreply.github.com>
2026-06-26 11:58:16 +0300

    chore: update securetrash to v0.4.7

--- Formula/securetrash.rb
+++ Formula/securetrash.rb
@@ -1,8 +1,8 @@
 class Securetrash < Formula
   desc "Honest secure file deletion for macOS (FileVault + crypto-shred vaults)"
   homepage "https://github.com/Di-kairos/securetrash"
-  url "https://github.com/Di-kairos/securetrash/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "234038c68d8c1086b909d12dc5f37eca01938aabbcc99c0d6824d83758230280"
+  url "https://github.com/Di-kairos/securetrash/archive/refs/tags/v0.4.7.tar.gz"
+  sha256 "8accac6724dc6a927a30c09de5301b2d20259eb55cd7b0a4dbfef8b0b9724102"
   license "MIT"
 
   def install
